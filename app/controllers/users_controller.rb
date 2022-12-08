905f475867d7facba50962ebaa727e0e7e887cda
sam9750 <sam.wadhwa95@gmail.com>
2022-12-08 17:00:30 -0500

    abc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     # skip_before_action :authorize, only: [:create]
     # wrap_parameters format: []
 
-    # def index
-    #   users = User.all
-    #   render json: users
-    # end
+    def index
+      users = User.all
+      render json: users
+    end
   
     def show
       user = User.find_by(id: session[:user_id])
