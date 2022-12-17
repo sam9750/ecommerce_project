b92516e5abdd2b0137ffea793c56e96ff08c0307
sam9750 <sam.wadhwa95@gmail.com>
2022-12-16 23:01:27 -0500

    readded heroku links

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -70,7 +70,7 @@ function App() {
 //   }, [])
 
   useEffect(() => {
-    fetch("/authorized_user").then((res) => {
+    fetch("https://ecommercesam.herokuapp.com/authorized_user").then((res) => {
       if (res.ok) {
         res.json().then((user) => {
           setIsAuthenticated(true);
@@ -86,7 +86,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch("/items").then((res) => {
+    fetch("https://ecommercesam.herokuapp.com/items").then((res) => {
       if (res.ok) {
         res.json().then((items) => {
           setItems(items);
@@ -103,7 +103,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch('/cart_items')
+    fetch('https://ecommercesam.herokuapp.com/cart_items')
       .then((res) => {
         if (res.ok) {
           res.json().then(cartItems => {
@@ -115,7 +115,7 @@ function App() {
   }, [user, cartItems.length ])
 
   useEffect(() => {
-    fetch('/cart')
+    fetch('https://ecommercesam.herokuapp.com/cart')
       .then((res) => {
         if (res.ok) {
           res.json().then(yourCartItems => setYourCartItems(yourCartItems))
